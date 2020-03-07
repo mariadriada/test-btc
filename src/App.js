c03ea69c9baae518182356a956751b2231b47f53
maria <mariagiraldo4@gmail.com>
2020-03-07 18:10:42 -0500

    Initial react project with sass and bootstrap

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Alert } from 'react-bootstrap'
+import './App.scss'
+
+function App() {
+  return (
+    <div className="App">
+      <Alert variant="success">
+        Hello... Boostrap ok!
+      </Alert>
+    </div>
+  );
+}
+
+export default App;
